708d2928e5bd41792fb7f13fafaf22857fec7216
lindo-kuhle10 <st10492029@rcconnect.edu.za>
2026-06-19 18:35:34 +0200

    Add files via upload

--- CyberSecurityBotSolution/CyberSecurityBot.GUI/obj/Debug/net10.0-windows/App.g.i.cs
+++ CyberSecurityBotSolution/CyberSecurityBot.GUI/obj/Debug/net10.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace CyberSecurityBot.GUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CyberSecurityBot.GUI {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
         public static void Main() {
             CyberSecurityBot.GUI.App app = new CyberSecurityBot.GUI.App();
             app.InitializeComponent();
